23123e26399c9f9a6335a9f446fc26409a7d6727
YosefSalave <yosef.salave@gmail.com>
2021-01-15 10:08:57 +0530

    new add

---
+++ src/test/java/MavenProject.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenProject 
+{
+	@Test
+	public void test()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		
+		System.out.println("Hello");
+	}
+
+}
